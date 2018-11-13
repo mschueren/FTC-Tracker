b7ade4bf285dec88e0fb54d0dff2a73b3cdb8671
Mikaela Caron <mikaelacaron@gmail.com>
2018-11-13 02:38:51 -0500

    Changes needed for network connections at CHS

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 <head>
 	<title>Login Page</title>
 	<link rel="stylesheet" type="text/css" href="main.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="jquery-google-api.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 <body>
